fe8bb11cd39b25b599ee10a1bb87a4d7b2c0da98
imloama <imloama@gmail.com>
2019-02-26 15:33:41 +0800

    feat: support electron

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,4 @@
+/// <reference path="globals/electron-builder/index.d.ts" />
+/// <reference path="globals/electron-devtools-installer/index.d.ts" />
+/// <reference path="globals/electron-packager/index.d.ts" />
 /// <reference path="globals/electron/index.d.ts" />
